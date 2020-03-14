7fe09fc2a6b4b693bfa1b35adf654ce90037b426
Brett S <brettrsears@gmail.com>
2020-03-14 10:15:16 -0500

    Updates

---
+++ precache-manifest.51897f48dcde7fb1a8aed335087f0f82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da623fd0f3593093d1837ad00b683572",
+    "url": "/reddit-is-awful/index.html"
+  },
+  {
+    "revision": "8c2f7203060796820d04",
+    "url": "/reddit-is-awful/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a4b04d6401f0be42878a",
+    "url": "/reddit-is-awful/static/js/2.aad93f56.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/reddit-is-awful/static/js/2.aad93f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2f7203060796820d04",
+    "url": "/reddit-is-awful/static/js/main.62dd19f1.chunk.js"
+  },
+  {
+    "revision": "9d60db84b78ca34748de",
+    "url": "/reddit-is-awful/static/js/runtime-main.1d096430.js"
+  },
+  {
+    "revision": "f3a53693fdbaf052e8f883c8fa2efcd6",
+    "url": "/reddit-is-awful/static/media/blog-post.1.f3a53693.md"
+  },
+  {
+    "revision": "99cbba3ba72c097ec32daa6f5eed33ec",
+    "url": "/reddit-is-awful/static/media/blog-post.2.99cbba3b.md"
+  },
+  {
+    "revision": "8d6e276be069ee011ba792969f01d327",
+    "url": "/reddit-is-awful/static/media/blog-post.3.8d6e276b.md"
+  },
+  {
+    "revision": "3a0e47367fde696e45001eabda853d9e",
+    "url": "/reddit-is-awful/static/media/ria.3a0e4736.png"
+  },
+  {
+    "revision": "8ad89815c1c93c9ea3ed93fd2098eda7",
+    "url": "/reddit-is-awful/static/media/riabackground.8ad89815.png"
+  }
+]);
